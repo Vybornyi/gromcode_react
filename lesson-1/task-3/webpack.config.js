df5cc4bcc2285ad80bc39b68f76bc2ac311ff29b
Vybornyi <1vybornyi@gmail.com>
2022-08-14 11:12:33 +0300

    add

--- lesson-1/task-3/webpack.config.js
+++ lesson-1/task-3/webpack.config.js
@@ -1,20 +1,19 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const webpack = require('webpack');
+const path = require('path');
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === 'production';
   const config = {
-    entry: './src/index.jsx',
+    entry: './src/index.js',
     output: {
-      filename: 'bundle.js',
+      path: path.join(__dirname, 'review_build'),
     },
     module: {
       rules: [
         {
-          test: /.(js|jsx?)$/,
-          exclude: /node_modules/,
+          test: /\.js$/,
           use: ['babel-loader'],
         },
         {
@@ -25,37 +24,34 @@ module.exports = (env, argv) => {
             'sass-loader',
           ],
         },
+        {
+          test: /.(jpg|png|gif)/,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                limit: 8192,
+                name: '[name].[ext]',
+                outputPath: 'images',
+              },
+            },
+          ],
+        },
       ],
     },
-    resolve: {
-      extensions: ['.js', '.jsx'],
-    },
     plugins: [
-      new webpack.ProgressPlugin(),
-      new CleanWebpackPlugin(),
       new HtmlWebpackPlugin({
         template: './src/index.html',
       }),
+      new CleanWebpackPlugin(),
     ],
     devServer: {
-      historyApiFallback: true,
-      open: true,
+      port: 9000,
       hot: true,
-      port: 8080,
     },
   };
-
-  if (isProduction) {
-    config.plugins.push(new webpack.HotModuleReplacementPlugin());
-  }
-
-  if (isProduction) {
-    config.plugins.push(
-      new MiniCssExtractPlugin({
-        filename: '[name].css',
-      }),
-    );
-  }
+  const productionPlugins = [new MiniCssExtractPlugin()];
+  if (isProduction) config.plugins.push(...productionPlugins);
 
   return config;
 };
