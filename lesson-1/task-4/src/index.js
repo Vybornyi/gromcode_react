53beac43069e37f6baa0d49b262cd30b50873677
Vybornyi <1vybornyi@gmail.com>
2022-08-14 11:32:22 +0300

    add

---
+++ lesson-1/task-4/src/index.js
@@ -0,0 +1,13 @@
+import './styles.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const rootElement = document.querySelector('#root');
+
+const element = (
+  <div className="greeting">
+    <h1 className="greeting__title">Hello, world!</h1>
+    <p className="greeting__text">I'm learning React</p>
+  </div>
+);
+ReactDOM.render(element, rootElement);
