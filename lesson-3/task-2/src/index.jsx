3e4e2d2c6ca0d8b2c7e46a656c674c15ab6f4534
Vybornyi <1vybornyi@gmail.com>
2022-08-16 12:32:20 +0300

    add

---
+++ lesson-3/task-2/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import './search.scss';
+import Search from './search';
+
+const rootElement = document.querySelector('#root');
+
+const Greeting = props => <Search name={props.name} />;
+ReactDOM.render(<Greeting name="Tom" />, rootElement);
