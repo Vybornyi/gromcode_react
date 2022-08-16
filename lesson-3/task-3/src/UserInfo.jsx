96febbab656bc2a9906b5f042c38e663a6a4b688
Vybornyi <1vybornyi@gmail.com>
2022-08-16 22:00:44 +0300

    add

--- lesson-3/task-3/src/UserInfo.jsx
+++ lesson-3/task-3/src/UserInfo.jsx
@@ -5,17 +5,14 @@ import './userInfo.scss';
 
 const formatDate = date => moment(date).format('DD MMM YYYY');
 
-const userInfo = props => {
-  console.log(props);
-  return (
-    <div className="comment">
-      <div className="user-info">
-        <Avatar name={props.user.name} avatarUrl={props.user.avatarUrl} />
-        <div className="user-info__name">{props.user.name}</div>
-      </div>
-      <div className="comment__text">{props.text}</div>
-      <div className="comment__date">{formatDate(props.date)}</div>
+const userInfo = props => (
+  <div className="comment">
+    <div className="user-info">
+      <Avatar name={props.user.name} avatarUrl={props.user.avatarUrl} />
+      <div className="user-info__name">{props.user.name}</div>
     </div>
-  );
-};
+    <div className="comment__text">{props.text}</div>
+    <div className="comment__date">{formatDate(props.date)}</div>
+  </div>
+);
 export default userInfo;
