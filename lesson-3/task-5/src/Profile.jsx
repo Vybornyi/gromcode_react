3f48ea052cd15a9f0d6abccda9f1c2b6213efaf4
Vybornyi <1vybornyi@gmail.com>
2022-08-17 16:04:12 +0300

    add

---
+++ lesson-3/task-5/src/Profile.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import moment from 'moment';
+import './index.scss';
+
+const formatDate = date => moment(date).format('DD MMM YY');
+const Profile = props => (
+  <>
+    <div className="profile__name">{`${props.userData.firstName} ${props.userData.lastName}`} </div>
+    <div className="profile__birth">{`Was born ${formatDate(props.userData.birthDate)} in ${
+      props.userData.birthPlace
+    }`}</div>
+  </>
+);
+
+export default Profile;
---
+++ lesson-3/task-5/src/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Profile from './Profile';
+
+const userData = {
+  firstName: 'James',
+  lastName: 'Bond',
+  birthDate: '1991-01-17T11:11:11.819Z',
+  birthPlace: 'London',
+};
+const profileElem = document.querySelector('.profile');
+
+ReactDOM.render(<Profile userData={userData} />, profileElem);
