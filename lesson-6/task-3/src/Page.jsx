795c10969296eb92158837ca0c425463a14be0a0
Vybornyi <1vybornyi@gmail.com>
2022-08-22 19:13:13 +0300

    add

---
+++ lesson-6/task-3/src/Page.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Message from './Message';
+import './index.scss';
+
+class Page extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: null,
+    };
+  }
+
+  setText = text => {
+    this.setState({
+      text,
+    });
+  };
+
+  render() {
+    return (
+      <div className="page">
+        <Message text={this.state.text} />
+        <div className="actions">
+          <button className="btn" onClick={() => this.setText('Hello, world!')}>
+            Text 1
+          </button>
+          <button className="btn" onClick={() => this.setText('Another exciting text')}>
+            Text 2
+          </button>
+          <button className="btn" onClick={() => this.setText('')}>
+            Clear
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Page;
---
+++ lesson-6/task-3/src/index.jsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import Page from './Page';
+
+const rootElem = document.querySelector('#root');
+
+ReactDOM.render(<Page />, rootElem);
