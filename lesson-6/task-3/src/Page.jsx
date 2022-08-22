bff32520052ac6ef3ab4723ed48d20b76d855c44
Vybornyi <1vybornyi@gmail.com>
2022-08-22 19:14:06 +0300

    add

--- lesson-6/task-3/src/Page.jsx
+++ lesson-6/task-3/src/Page.jsx
@@ -24,7 +24,7 @@ class Page extends React.Component {
           <button className="btn" onClick={() => this.setText('Hello, world!')}>
             Text 1
           </button>
-          <button className="btn" onClick={() => this.setText('Another exciting text')}>
+          <button className="btn" onClick={() => this.setText('Another exciting text.')}>
             Text 2
           </button>
           <button className="btn" onClick={() => this.setText('')}>
