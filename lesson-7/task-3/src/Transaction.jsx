dbab03f32a9e08ef8f7f0c49b6d209b0a312e782
Vybornyi <1vybornyi@gmail.com>
2022-08-23 22:12:50 +0300

    add

--- lesson-7/task-3/src/Transaction.jsx
+++ lesson-7/task-3/src/Transaction.jsx
@@ -3,7 +3,7 @@ import moment from 'moment';
 
 const getDay = date => moment(date).format('D MMM');
 const getTime = date => moment(date).format(' HH:mm');
-const numberFormat = number => new Intl.NumberFormat('de-DE').format(number);
+const numberFormat = number => new Intl.NumberFormat('en-GB').format(number);
 
 const Transaction = ({ from, to, amount, rate, time }) => (
   <li className="transaction">
