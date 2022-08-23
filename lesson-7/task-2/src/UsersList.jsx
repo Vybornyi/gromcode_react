5fa784f00c0d6121b199bfd102f6f4b33ca86ec1
Vybornyi <1vybornyi@gmail.com>
2022-08-23 20:55:04 +0300

    add

---
+++ lesson-7/task-2/src/UsersList.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import User from './User';
+import './index.scss';
+
+class UsersList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sort: null,
+    };
+  }
+
+  toggleSorting = () => {
+    const newSorting = this.state.sort === 'asc' ? 'desc' : 'asc';
+    this.setState({
+      sort: newSorting,
+    });
+  };
+
+  render() {
+    let sortUsers;
+    if (this.state.sort) {
+      sortUsers = this.props.users
+        .slice()
+        .sort((a, b) => (this.state.sort === 'asc' ? a.age - b.age : b.age - a.age));
+    } else {
+      sortUsers = this.props.users;
+    }
+
+    return (
+      <div>
+        <button onClick={this.toggleSorting} className="btn">
+          {this.state.sort || '-'}
+        </button>
+        <ul className="users">
+          {sortUsers.map(user => (
+            <User key={user.id} name={user.name} age={user.age} />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default UsersList;
--- lesson-7/task-2/src/index.jsx
+++ lesson-7/task-2/src/index.jsx
@@ -1,7 +1,28 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import Auth from './Auth';
+import UsersList from './UsersList';
 
 const rootElem = document.querySelector('#root');
-
-ReactDOM.render(<Auth />, rootElem);
+const users = [
+  {
+    id: 'id-0',
+    age: 21,
+    name: 'Bob',
+  },
+  {
+    id: 'id-1',
+    age: 17,
+    name: 'Tom',
+  },
+  {
+    id: 'id-2',
+    age: 18,
+    name: 'Tad',
+  },
+  {
+    id: 'id-3',
+    age: 45,
+    name: 'Sam',
+  },
+];
+ReactDOM.render(<UsersList users={users} />, rootElem);
