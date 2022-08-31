fc91e7729054db4ef8d1a54ef8b95bfd08e1a918
Vybornyi <1vybornyi@gmail.com>
2022-08-31 13:21:49 +0300

    add

---
+++ lesson-9/task-1/src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Search from './Search';
+
+const rootElement = document.querySelector('#root');
+ReactDOM.render(<Search />, rootElement);
